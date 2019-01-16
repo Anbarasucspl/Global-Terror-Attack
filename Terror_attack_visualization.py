f877a3c2b087721f0b62fdbe2dc8ac636b1de533
Anbarasucspl <anbarasu.mahadev@gmail.com>
2019-01-16 13:26:46 +0530

    Update Terror_attack_visualization.py

--- Terror_attack_visualization.py
+++ Terror_attack_visualization.py
@@ -3,23 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 class globalterror:
-    def last10yrvictim(self,data,country,xval,yval):
-        plt.figure(figsize=(15,7))
-        country_name=data['country_txt'].unique()
+    def bargraphplot(self,data,xval,yval,xlabel,ylabel):
+        plt.figure(figsize=(15, 7))
         sns.barplot(x=data[xval].values,y=data[yval].values)
         plt.xticks(rotation=90)
-        plt.xlabel('Year')
-        plt.ylabel('Total No of Victims')
-        plt.title('Terror victim Chart : ' + country_name)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title('Terror victim Chart')
         plt.show()
+    def linegraphplot(self,datafrm,index_val):
+        datafrm.set_index(index_val).plot(marker='o')
+        plt.gcf().set_size_inches((15,7),forward=True)
+        
+        
 global_terr_data=pd.read_csv('D:\Python\gtd\globalterrorism.csv')
-Terror_plot_year_wise=global_terr_data.groupby(['country_txt','iyear']).sum()
-country_10yrvictim=[input('Enter the country name for details:')]
-Country_wise_nkill=Terror_plot_year_wise['nkill'].reset_index().rename({'nkill':'Total_kills'},axis='columns')
-Country_nkill=Country_wise_nkill[Country_wise_nkill['country_txt'].isin(country_10yrvictim)].sort_values(by='iyear',ascending=False)[:input('Enter number of years details you want to view(max47):')]
 obj_globalterror=globalterror()
-obj_globalterror.last10yrvictim(Country_nkill,country_10yrvictim,'iyear','Total_kills')
+Interested_country=[input('Enter the country name for details:')]
+Terror_plot_year_wise=global_terr_data.groupby(['country_txt','iyear']).sum()
+Terror_kills_global=Terror_plot_year_wise['nkill'].reset_index()
+Terror_cnt_slct_conty=Terror_kills_global[Terror_kills_global['country_txt'].isin(Interested_country)].reset_index()
+Terror_count_India=Terror_cnt_slct_conty[['iyear','nkill']]
+obj_globalterror.linegraphplot(Terror_count_India,'iyear')
+Terror_plot_global_year_wise=global_terr_data.groupby(['iyear']).sum()
+Terror_kill_global=Terror_plot_global_year_wise['nkill'].reset_index()
+Terror_count_global=Terror_kill_global[['iyear','nkill']]
+obj_globalterror.linegraphplot(Terror_count_global,'iyear')
 Terror_attack_type_wise=global_terr_data.groupby(['country_txt','attacktype1_txt']).count()
-attact_type_count=Terror_attack_type_wise['iyear'].reset_index()
-attack_type=attact_type_count[attact_type_count['country_txt'].isin(country_10yrvictim)]
-obj_globalterror.last10yrvictim(attack_type,country_10yrvictim,'attacktype1_txt','iyear')
+attact_type_count=Terror_attack_type_wise['iyear'].reset_index().rename({'iyear':'Total_count'}, axis='columns')
+attack_type_slct_cnty=attact_type_count[attact_type_count['country_txt'].isin(Interested_country)]
+obj_globalterror.bargraphplot(attack_type_slct_cnty,'attacktype1_txt','Total_count','Types of attack','victim numbers')
+attack_type_cnt_global=attact_type_count.groupby(['attacktype1_txt']).sum().reset_index()
+obj_globalterror.bargraphplot(attack_type_cnt_global,'attacktype1_txt','Total_count','Types of attack','victim numbers')
